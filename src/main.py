ebe9946b312e0aa9f0fe053dd898e8a0bf9e2113
V_A_N <V_A_N-1_94@mail.ru>
2023-10-25 19:49:08 +0300

    Create dict card and account

--- src/main.py
+++ src/main.py
@@ -2,7 +2,21 @@ def main():
     from src.masks import get_mask_card, returning_account_mask
 
     numbers_card = "7000792289606361"
+
     number_account = "73654108430135874305"
+
+    numbers_card_dict = {
+        "Maestro": 1596837868705199,
+        "MasterCard": 7158300734726758,
+        "Visa Classic": 6831982476737658,
+        "Visa Platinum": 8990922113665229,
+        "Visa Gold": 5999414228426353,
+        }
+    number_account_dict = {
+        "Счет": 64686473678894779589,
+        "Счет": 35383033474447895560,
+        "Счет": 73654108430135874305
+        }
 
     print(get_mask_card(numbers_card))
     print(returning_account_mask(number_account))
