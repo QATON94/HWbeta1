e324efa93cbadb4f3dc38234003a70ade90b7a0b
V_A_N <V_A_N-1_94@mail.ru>
2023-10-29 16:37:26 +0300

    Correction of code

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from src.processing import choice_state, sort_date
 
 
-def main():
+def main() -> None:
     list_data = [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -9,7 +9,7 @@ def main():
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
 
-    print(choice_state(list_data, "EXECUTED"))
+    print(choice_state(list_data))
     print(sort_date(list_data))
 
 
--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 
-def choice_state(list_dict: list, state: str) -> list:
+def choice_state(list_dict: list[dict], state: str = 'EXECUTED') -> list[dict]:
     """
     Функция возврощает список словорей у которых ключ
     state содержит переданное в функцию значение
@@ -14,7 +14,12 @@ def choice_state(list_dict: list, state: str) -> list:
     return state_dict
 
 
-def sort_date(list_dict: list, reverse_: bool = True) -> list:
-    """Функция возврощает отсортированный словарь по дате"""
+def sort_date(list_dict: list[dict], reverse_: bool = True) -> list[dict]:
+    """
+    Функция сортировки списка операций по дате.
+    :param list_dict: Список словарей для сортировки
+    :param reverse_: Параметр направления сортировки. По-умолчанию - True
+    :return: Отсортированный список операций по дате
+    """
     sort_list = sorted(list_dict, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"), reverse=reverse_)
     return sort_list
