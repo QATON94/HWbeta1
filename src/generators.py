4c7aba6346b701cddf029f1146a92bed59889240
V_A_N <V_A_N-1_94@mail.ru>
2023-11-06 17:53:15 +0300

    add generators.py  card_number_generator function

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,5 @@
-def filter_by_currency(transactions:list, currency):
+def filter_by_currency(transactions:list, currency:str) -> int:
+
     for item in transactions:
         if currency == item['operationAmount']["currency"]["name"]:
             yield item
@@ -7,3 +8,11 @@ def filter_by_currency(transactions:list, currency):
 def transaction_descriptions(transactions:list):
     for item in transactions:
         yield item["description"]
+
+
+def card_number_generator(start, end):
+    num = '0000000000000000'
+    for number in range(start, end+1):
+        number = str(number)
+        num = num[:-(len(number))] + number
+        yield ' '.join(num[i:i+4] for i in range(0, len(num), 4))
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from src.generators import filter_by_currency, transaction_descriptions
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 import json
 
 
@@ -16,6 +16,9 @@ def main() -> None:
     for _ in range(5):
         print(next(descriptions))
 
+
+    for card_number in card_number_generator(1, 5):
+        print(card_number)
 
 
 if __name__ == "__main__":
