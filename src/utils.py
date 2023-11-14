cce860e9e455f2447053d1611659f7aa184ac076
V_A_N <V_A_N-1_94@mail.ru>
2023-11-14 19:40:17 +0300

    add utils.py function

--- src/utils.py
+++ src/utils.py
@@ -11,15 +11,17 @@ def transaction_amount(path_: str) -> float | list:
             transactions = json.load(file)
     except json.JSONDecodeError as error:
         return []
-
-    amount_sum = 0
-    for transaction in transactions:
-        try:
-            if transaction["operationAmount"]["currency"]["code"] == "RUB":
-                value_amount = transaction["operationAmount"]["amount"]
-                amount_sum += float(value_amount)
-        except ValueError:
-            print("Транзация выполнена не в рублях. Укажите транзакцию в рублях")
-        except KeyError:
-            print("Неверная сумма транзакции")
-    return round(amount_sum, 2)
+    if transactions == []:
+        return []
+    else:
+        amount_sum = 0
+        for transaction in transactions:
+            try:
+                if transaction["operationAmount"]["currency"]["code"] == "RUB":
+                    value_amount = transaction["operationAmount"]["amount"]
+                    amount_sum += float(value_amount)
+            except ValueError:
+                print("Транзация выполнена не в рублях. Укажите транзакцию в рублях")
+            except KeyError:
+                print("Неверная сумма транзакции")
+        return round(amount_sum, 2)
