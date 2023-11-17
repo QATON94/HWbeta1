da2ab9b70780e02e71f7ed823c4648d9dfc2cfb7
V_A_N <V_A_N-1_94@mail.ru>
2023-11-18 01:46:26 +0300

    add logging

--- src/main.py
+++ src/main.py
@@ -1,29 +1,28 @@
 from pathlib import Path
 
+from src.logger import setup_logging
 from src.utils import transaction_amount, transaction_json
+from src.widget import get_mask_cards_and_accounts
+
+logger = setup_logging()
 
 
 def main() -> None:
+    logger.info(["Запуск программы..."])
+
     ROOT_PATH = Path(__file__).parent.parent
     OPERATIONS_JSON = ROOT_PATH.joinpath("data", "operations.json")
-    # with open(os.path.join("..", "data", "transactions.json"), "r", encoding="UTF-8") as file:
-    #     transactions = json.load(file)
-    #
-    # usd_transactions = filter_by_currency(transactions, "руб.")
-    #
-    # for _ in range(2):
-    #     print(next(usd_transactions)["id"])
-    #
-    # descriptions = transaction_descriptions(transactions)
-    #
-    # for _ in range(5):
-    #     print(next(descriptions))
-    #
-    # for card_number in card_number_generator(1, 5):
-    #     print(card_number)
 
     transactions = transaction_json(OPERATIONS_JSON)
-    print(transaction_amount(transactions[0]))
+    if transactions != []:
+        result = transaction_amount(transactions[1])
+        print(result)
+        card = get_mask_cards_and_accounts(transactions[1]["from"])
+        account_number = get_mask_cards_and_accounts(transactions[1]["to"])
+        print(card)
+        print(account_number)
+
+    logger.info(["Завершение программы..."])
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,35 +1,46 @@
 import json
-from typing import Any
+import logging
+from typing import Any, Union
 
 import requests
 
+logger = logging.getLogger(__name__)
 
-def transaction_json(json_file_path: Any) -> list[Any] | dict[Any, Any]:
+
+def transaction_json(json_file_path: Any) -> Union[list[Any] | dict[Any, Any]]:
     """Функция возврощается список транзакций или пустой список
     param path_: Путь к json файлу с транзакциями
     return: возврощает пустой список если файл не найден, либо список транзакций
     """
     try:
+        logger.info(f"Открывается файл: {json_file_path}")
         with open(json_file_path, "r", encoding="UTF-8") as file:
             transactions = json.load(file)
         return transactions
-    except json.JSONDecodeError:
-        return []
     except FileNotFoundError:
+        logger.error("Файл не найден FileNotFoundError")
+        return []
+    except json.JSONDecodeError:
+        logger.error("файл JSON не соответствуют правильному формату: FileNotFoundError")
         return []
 
 
-def transaction_amount(transaction:list[dict]) -> float:
+def transaction_amount(transaction: Any) -> float:
     """Функция возврощает сумму транзакций в рублях, если не в рублях выводит ошибку
     :param transaction: Словарь с данными о транзакции
     :return сумма транзакции
     :raise Ошбика если транзакция не в рублях
     """
     if transaction["operationAmount"]["currency"]["code"] == "RUB":
+        amount_rate = float(transaction["operationAmount"]["amount"])
+        logger.info(f"Сумма транзакции: {amount_rate}")
         return float(transaction["operationAmount"]["amount"])
     else:
+
         cash_rate = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()["Valute"]
         name_code = transaction["operationAmount"]["currency"]["code"]
+        logger.info(f"Получаем информацию курса {name_code}")
         value = cash_rate[name_code]['Value']
         amount_rate = round(float(value) * float(transaction["operationAmount"]["amount"]), 2)
+        logger.info(f"Сумма транзакции: {amount_rate}")
         return amount_rate
